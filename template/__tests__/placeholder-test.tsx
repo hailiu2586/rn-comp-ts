7619153fc22071b325330326d2d0d358124c511d
Hai Liu <hailiu@gmail.com>
2022-04-18 09:51:50 -0700

    Create placeholder-test.tsx

---
+++ template/__tests__/placeholder-test.tsx
@@ -0,0 +1,14 @@
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ */
+
+import "react-native";
+import React from "react";
+
+// Note: test renderer must be required after react-native.
+import renderer from "react-test-renderer";
+
+jest.useFakeTimers();
+
+it("renders correctly", () => {
+});
